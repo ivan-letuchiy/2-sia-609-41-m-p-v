04720b5035c6959657300c08a6a3b557f08c961c
ivan-letuchiy <letuchiyecosystem@gmail.com>
2025-10-07 10:07:32 +0400

    Start project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,70 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <header>
+    <nav>
+      <div v-if="isAuthenticated && user">
+        Welcome, {{ user.name }}
+        <button @click="logout">Logout</button>
+      </div>
+      <div v-else>
+        <form @submit.prevent="login">
+          <div>
+            <label for="email">Email</label>
+            <input v-model="email" type="email" id="email" required/>
+          </div>
+          <div>
+            <label for="password">Password</label>
+            <input v-model="password" type="password" id="password" required/>
+          </div>
+          <button type="submit">Login</button>
+          <p v-if="authError" class="error">{{ authError }}</p>
+        </form>
+      </div>
+    </nav>
+  </header>
 </template>
 
-<style scoped></style>
+<script>
+import {useAuthStore} from "@/stores/authStore.js";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      authStore: useAuthStore(),
+    };
+  },
+  computed: {
+    isAuthenticated() {
+      return this.authStore.isAuthenticated;
+    },
+    user() {
+      return this.authStore.user;
+    },
+    authError() {
+      return this.authStore.errorMessage;
+    },
+  },
+  methods: {
+    login() {
+      this.authStore.login({email: this.email, password: this.password});
+    },
+    logout() {
+      this.authStore.logout();
+    },
+  },
+  mounted() {
+    const token = localStorage.getItem("token");
+    if (token) {
+      this.authStore.isAuthenticated = true;
+      this.authStore.getUser();
+    }
+  },
+};
+</script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
